0a410d6a48f4c921dbf9d16f06b4f4eb9d90bd92
dongle.qb.2001@gmail.com <dongle.qb.2001@gmail.com>
2021-06-21 10:12:48 +0700

    add nodemon, nest configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { configService } from './config/config.service';
+
+const PORT = configService.getPort() || 5000
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT , () => console.log(`api listening on port ${PORT}`));
 }
+
 bootstrap();
