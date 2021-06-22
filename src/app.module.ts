0d3baf326017019158aa0210571e326791a96267
dongle.qb.2001@gmail.com <dongle.qb.2001@gmail.com>
2021-06-22 09:48:10 +0700

    update .gitignore, add cors, setup swagger api docs

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,15 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { configService } from './config/config.service'
+import { configService } from './config/config.service';
+import { ItemModule } from './item/item.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(configService.getTypeOrmConfig())
+    TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
+    ItemModule
   ],
-  controllers: [AppController],
   providers: [AppService],
+  controllers: [AppController],
 })
 export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,34 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { configService } from './config/config.service';
 
 const PORT = configService.getPort() || 5000
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(PORT , () => console.log(`api listening on port ${PORT}`));
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: true,
+      preflightContinue: false,
+    }
+  });
+  
+  if (!configService.isProduction()) {
+
+    const document = SwaggerModule.createDocument(app, new DocumentBuilder()
+      .setTitle('Item API')
+      .setDescription('My Item API')
+      .build());
+
+    SwaggerModule.setup('docs', app, document);
+
+  }
+  
+  // Auto-validation - use for auto transform body to DTO
+  app.useGlobalPipes(new ValidationPipe());
+
+  await app.listen(PORT, () => console.log(`api listening on port ${PORT}`));
 }
 
 bootstrap();
