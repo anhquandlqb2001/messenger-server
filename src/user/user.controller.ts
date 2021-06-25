664a85b2081799316d6557152686cdf3bdcf64b0
dongle.qb.2001@gmail.com <dongle.qb.2001@gmail.com>
2021-06-25 08:07:12 +0700

    add full-text-search user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,16 +1,29 @@
 import { Controller, Get, Request, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
-import { JwtAuthGuard } from '../auth/jwt-auth.guard'
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) { }
+  constructor(private readonly userService: UserService) {}
 
   @UseGuards(JwtAuthGuard)
   @Get()
   async profile(@Request() req) {
-    const user = await this.userService.findUser(req.user.email)
-    const { password, ...result } = user
-    return result
+    const user = await this.userService.findUser(req.user.email);
+    const { password, ...result } = user;
+    return {
+      user: result,
+      success: true,
+    };
+  }
+
+  // @UseGuards(JwtAuthGuard)
+  @Get('search')
+  async findPeople(@Request() req) {
+    const users = await this.userService.findOtherPeople(req.body.searchTerm);
+    return {
+      users: users,
+      success: true,
+    };
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,34 +1,51 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '../entities/user.entity';
-import { Repository } from 'typeorm';
+import { getConnection, Repository } from 'typeorm';
 import { UserDTO } from './user.dto';
-import * as bcrypt from 'bcrypt'
+import * as bcrypt from 'bcrypt';
 import { SALT_ROUND } from '../constants';
+import { PublicUserDTO } from './public-user.dto';
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(User)
-    private readonly userRepository: Repository<User>
-  ) { }
+    private readonly userRepository: Repository<User>,
+  ) {}
 
   public async createUser(userDto: UserDTO) {
     try {
       // hash password
       userDto.password = await bcrypt.hash(userDto.password, SALT_ROUND);
 
-      const user = await this.userRepository.save(userDto.toEntity())
+      const user = await this.userRepository.save(userDto.toEntity());
 
-      const { email, firstName, lastName } = user
-      return { email, firstName, lastName }
+      return PublicUserDTO.createFromEntity(user)
     } catch (error) {
       console.log(error);
-      return { error }
+      return { error };
     }
   }
 
+  // need password
   public async findUser(email: string): Promise<User | undefined> {
-    return this.userRepository.findOne({ where: { email } })
+    return this.userRepository.findOne({ where: { email } });
+  }
+
+  public async findOtherPeople(searchTerm: string) {
+    const users = await this.userRepository
+      .createQueryBuilder('users')
+      .select()
+      .where('users.firstName ILIKE (:searchTerm)', {
+        searchTerm: `%${searchTerm}%`,
+      })
+      .orWhere('users.lastName ILIKE (:searchTerm)', {
+        searchTerm: `%${searchTerm}%`,
+      })
+      .getMany();
+
+    const result = users.map((user) => PublicUserDTO.createFromEntity(user));
+    return result;
   }
 }
