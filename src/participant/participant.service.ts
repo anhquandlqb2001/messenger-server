cfd9444b1072f7623c1d1def73ba2b9658fd5027
dongle.qb.2001@gmail.com <dongle.qb.2001@gmail.com>
2021-06-27 15:10:19 +0700

    switch to using transaction

--- src/participant/participant.service.ts
+++ src/participant/participant.service.ts
@@ -21,18 +21,14 @@ export class ParticipantService {
     participantIds,
     participantType,
   }: FindConversationType) {
-    const result = await getManager().query(
-      `SELECT participant."conversationId" FROM participants as participant WHERE participant."userId" = $1 AND participant."type" = $3
-        INTERSECT
-		    SELECT participant_1."conversationId" FROM participants as participant_1 WHERE participant_1."userId" = ANY($2) AND participant_1."type" = $3`,
-      [creatorId, participantIds, participantType],
+    const result = await getManager().transaction((em) =>
+      em.query(
+        `SELECT participant."conversationId" FROM participants as participant WHERE participant."userId" = $1 AND participant."type" = $3
+      INTERSECT
+      SELECT participant_1."conversationId" FROM participants as participant_1 WHERE participant_1."userId" = ANY($2) AND participant_1."type" = $3`,
+        [creatorId, participantIds, participantType],
+      ),
     );
-    /**
- * 
-		INTERSECT
-		SELECT 'conversationId' FROM participants WHERE 'userId' = ANY($2)
- */
-    console.log(result);
     if (result.lenght < 1) {
       throw new NotFoundException();
     }
